97418a545011f8d0df1a382cccef3649f643dd62
Shinji Yamada <dotneet@gmail.com>
2026-09-23 21:37:29 +0900

    Keep module references out of class prefixing

--- crates/typer/src/prefix.rs
+++ crates/typer/src/prefix.rs
@@ -557,20 +557,13 @@ impl SymbolTable {
         };
         if !crate::symbol::any_type(
             &ty,
-            &mut |t| matches!(t, Type::Class { sym, .. } | Type::ModuleRef(sym) if wants(self, *sym)),
+            &mut |t| matches!(t, Type::Class { sym, .. } if wants(self, *sym)),
         ) {
             return ty;
         }
         let on_class = |c: Type| -> Type {
             match &c {
                 Type::Class { sym, .. } if wants(self, *sym) => with_prefix(c, pre.clone()),
-                Type::ModuleRef(sym) if wants(self, *sym) => with_prefix(
-                    Type::Class {
-                        sym: *sym,
-                        args: vec![],
-                    },
-                    pre.clone(),
-                ),
                 _ => c,
             }
         };
@@ -600,7 +593,6 @@ fn map_views(
 ) -> Type {
     let go = |t: &Type| map_views(t, on_class, on_prefix);
     match ty {
-        Type::ModuleRef(_) => on_class(ty.clone()),
         Type::Class { sym, args } => on_class(Type::Class {
             sym: *sym,
             args: args.iter().map(go).collect(),
